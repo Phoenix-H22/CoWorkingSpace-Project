1776d4ff4fb6b3aefdb85445049d808bb3a7c0db
Abdalrhman M. Alkady <alkady2019@gmail.com>
2023-02-22 14:47:00 +0200

    edits

--- resources/views/dashboard/admin/layout/footer.blade.php
+++ resources/views/dashboard/admin/layout/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="bg-secondary rounded-top p-4">
         <div class="row">
             <div class="col-12 col-sm-6 text-center text-sm-start">
-                &copy; <a href="#">Alien WorkSpace</a>  All Right Reserved. 
+                &copy; <a href="#">{{env("APP_NAME")}} WorkSpace</a>  All Right Reserved.
             </div>
         </div>
     </div>
