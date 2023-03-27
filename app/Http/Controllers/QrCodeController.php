733fb500730113665533abbff7d49b68a933546c
Abdalrhman M. Alkady <alkady2019@gmail.com>
2023-03-27 20:14:05 +0200

    Update QrCodeController.php

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -5,20 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use chillerlan\QRCode\QRCode;
 use Illuminate\Support\Facades\Http;
-
+use App\Http\Controllers\api\SessionsController;
 class QrCodeController extends Controller
 {
     public function index(Request $request)
     {
-        $response =    Http::withHeaders(
-            [
-                'X-CSRF-TOKEN' => csrf_token(),
-            ]
-        )->post('https://192.168.1.101/CoWorkingSpace-Project/public/api/scan', [
-            'card_id' => $request->card_id,
-        ]);
-        $response = json_decode($response);
-        return $response;   
+        // SessionsController::scan($request);
+        return response()->json(['data' => 'done'], 200);
 
     }
     public function cam(){
