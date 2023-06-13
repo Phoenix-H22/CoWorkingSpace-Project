d14afb0f06dc39a78ab343e992b84cd556f04006
Abdalrhman M. Alkady <alkady2019@gmail.com>
2023-06-13 21:54:57 +0300

    Solve js problem

--- resources/views/dashboard/admin/layout/footer.blade.php
+++ resources/views/dashboard/admin/layout/footer.blade.php
@@ -18,8 +18,8 @@
 </div>
 
 <!-- JavaScript Libraries -->
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.min.js"></script>
 <script src="{{asset("dashboard/assets/lib/chart/chart.min.js")}}"></script>
 <script src="{{asset("dashboard/assets/lib/easing/easing.min.js")}}"></script>
 <script src="{{asset("dashboard/assets/lib/waypoints/waypoints.min.js")}}"></script>
